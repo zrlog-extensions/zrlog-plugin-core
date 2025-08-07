53faed6707dafcb7c51e44f0b4e998aa7b23f39f
xiaochun <90xchun@gmail.com>
2025-08-07 12:24:42 +0800

    print ms

--- src/main/java/com/zrlog/plugincore/server/Application.java
+++ src/main/java/com/zrlog/plugincore/server/Application.java
@@ -40,7 +40,7 @@ public class Application {
     }
 
     public static void init() {
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %5$s%6$s%n");
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$s %5$s%6$s%n");
         FileHandler fileHandler = com.hibegin.common.util.LoggerUtil.buildFileHandle();
         LoggerUtil.initFileHandle(fileHandler);
         com.hibegin.common.util.LoggerUtil.initFileHandle(fileHandler);
